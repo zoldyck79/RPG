2e1e90f5cd11d264b12bbe7c675d2f77beaa8c6f
Zoldyck78 <125039244+Zoldyck78@users.noreply.github.com>
2023-02-13 01:37:37 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import fs from 'fs';
 import crypto from 'crypto'
 let handler = async (m, { conn, text, usedPrefix }) => {
-    const ultah = new Date('November 4 2022 23:59:59')
+    const ultah = new Date('October 15 2023 23:59:59')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
@@ -23,16 +23,16 @@ ${botdate}
 `
         let img1 = fs.readFileSync('./thumbnail.jpg');
         let img2 = fs.readFileSync('./media/bawah.png');
-    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/6ab4daac226292a112540.jpg`)).buffer(), [["Menu", usedPrefix + "menu"], ["Sewa Bot", usedPrefix + "sewa"]], m, {
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/6ab4daac226292a112540.jpg`)).buffer(), [["Menu", usedPrefix + "menu"]], m, {
         contextInfo: {
             externalAdReply: {
                 mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
-                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        title: ` Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥 𝙏𝙚𝙖𝙢`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://pin.it/7xb00eG`)).buffer(),
+                        sourceUrl: 'https://pin.it/7xb00eG'
             }
         }
     })
